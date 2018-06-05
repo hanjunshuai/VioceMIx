275b78d9497b3db152109479a9a1800c31a3130b
unknown <1433452759@qq.com>
2018-06-05 14:23:11 +0800

    finish 发音有点难听

--- app/src/main/java/hjs/zhi/com/voicemixdemo/MainActivity.java
+++ app/src/main/java/hjs/zhi/com/voicemixdemo/MainActivity.java
@@ -1,13 +1,74 @@
 package hjs.zhi.com.voicemixdemo;
 
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+    private static final int CHECK_TTS_ENGINE = 0;
+    private static final String TAG = "MainActivity";
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.speak).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tts.speak("Hello World", TextToSpeech.QUEUE_FLUSH, null);
+                Log.e(TAG, "onClick: 播放");
+
+            }
+        });
+        Intent intent = new Intent();
+        intent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(intent, CHECK_TTS_ENGINE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CHECK_TTS_ENGINE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                tts = new TextToSpeech(this, this);
+                Log.e(TAG, "onActivityResult: 安装成功");
+            } else {
+                Intent installIntent = new Intent();
+                installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installIntent);
+                Log.e(TAG, "onActivityResult: 未安装去安装");
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onInit(int i) {
+        if (i == TextToSpeech.SUCCESS) {
+            if (tts.isLanguageAvailable(Locale.US) == TextToSpeech.LANG_AVAILABLE)
+                Log.e(TAG, "onInit: SUCCESS");
+            tts.setLanguage(Locale.US);
+        } else if (i == TextToSpeech.ERROR) {
+            Log.e(TAG, "onInit: Error");
+            Toast.makeText(this, "Sorry! Text To Speech failed...",
+                    Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        tts.shutdown();//释放资源
+        Log.e(TAG, "onDestroy: 释放资源");
     }
 }
